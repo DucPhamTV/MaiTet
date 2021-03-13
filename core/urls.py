352f6206995f771e272ccbbd1358936072aa4eeb
DucPhamTV <phamcongduc1994@gmail.com>
2021-03-13 04:56:06 +0000

    Add serializer for Comment model

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from rest_framework import routers
 from v1.users.views import UserViewSet
 from v1.items.views import ItemViewSet
+from v1.comments.views import CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'items', ItemViewSet)
+router.register(r'comments', CommentViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- v1/comments/views.py
+++ v1/comments/views.py
@@ -1,3 +1,14 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+
+from v1.comments.serializers import CommentSerializer
+from v1.comments.models import Comment
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
