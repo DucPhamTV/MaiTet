136ba2bfa9c8dbc63d8a89b2f3ddc19c92bf0fd4
DucPhamTV <phamcongduc1994@gmail.com>
2021-03-20 03:12:21 +0000

    Basic token authentication with djoser

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -112,8 +114,18 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+}
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': False,
+    'SERIALIZERS': {},
 }
 
 # Internationalization
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 from rest_framework import routers
 from v1.users.urls import router as user_router
 from v1.items.urls import router as item_router
@@ -7,6 +8,7 @@ from v1.comments.urls import router as comment_router
 from v1.addresses.urls import router as address_router
 
 router = routers.DefaultRouter()
+
 router.registry.extend(user_router.registry)
 router.registry.extend(item_router.registry)
 router.registry.extend(comment_router.registry)
@@ -18,4 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('auth/', include("djoser.urls")),
+    #path('auth/', include('djoser.urls.authtoken')),
+    url(r'^authen/', include('djoser.urls')),
+    url(r'^authen/', include('djoser.urls.authtoken')),
 ]
