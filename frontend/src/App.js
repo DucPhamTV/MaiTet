87be70b017c6d3a92fed3e06fb0b33de162e6739
DucPhamTV <phamcongduc1994@gmail.com>
2021-04-30 22:43:15 +0700

    Basic frontend react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 // frontend/src/App.js
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
@@ -9,6 +12,9 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <Switch>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
             <Route exact path="/" component={Home} />
           </Switch>
         </BrowserRouter>
