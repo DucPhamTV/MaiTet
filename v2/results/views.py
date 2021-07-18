b7f5d8e56776f1ea831db8e68195d35d01d55713
DucPhamTV <phamcongduc1994@gmail.com>
2021-07-18 07:07:13 +0000

    Result is read only

--- v2/results/views.py
+++ v2/results/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 from rest_framework import permissions
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from v2.results.serializers import ResultSerializer
 from v2.results.models import Result
 # Create your views here.
 
-class ResultViewSet(ModelViewSet):
+class ResultViewSet(ReadOnlyModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(created_by=self.request.user)
+        #return self.queryset.filter(tracker__pk=self.request.)
+        return self.queryset
